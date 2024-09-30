fbb877b42f416c5e6900668ef48158c38d38bc5d
boandriy <andriy.borovets@pecodesoftware.com>
2024-09-30 20:09:17 +0300

    add useCallback

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,41 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import IndustryCard from "./components/IndustryCard";
 import { Company } from "./types/types";
 import { companiesService } from "./services/companies";
 import { prepareIndustries } from "./utils/industry";
 
 function App() {
+  const [isError, setIsError] = useState(false);
   const [companies, setCompanies] = useState<Company[]>([]);
 
-  const fetchCompanies = async () => {
+  const fetchCompanies = useCallback(async () => {
     try {
       const result = await companiesService.getCompanies();
 
       setCompanies(result.data);
     } catch (error) {
+      setIsError(true);
       console.log("Fetching companies error: ", error);
     }
-  };
+  }, []);
 
   useEffect(() => {
     fetchCompanies();
-  }, []);
+  }, [fetchCompanies]);
 
   return (
     <div className="bg-gray-300 min-h-screen p-10">
-      <div className="grid grid-cols-3 gap-4 items-start">
-        {prepareIndustries(companies).map((industry) => (
-          <IndustryCard industry={industry} key={industry.id} />
-        ))}
-      </div>
+      {!isError ? (
+        <div className="grid grid-cols-3 gap-4 items-start">
+          {prepareIndustries(companies).map((industry) => (
+            <IndustryCard industry={industry} key={industry.id} />
+          ))}
+        </div>
+      ) : (
+        <div className="text-red-500 text-center text-lg">
+          Failed to fetch data
+        </div>
+      )}
     </div>
   );
 }
